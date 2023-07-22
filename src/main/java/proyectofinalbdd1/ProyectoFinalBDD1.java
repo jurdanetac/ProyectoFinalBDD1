7e747263f721469a9481b3ef9b9345c61c4338b9
Juan Urdaneta <jurdanetac10@gmail.com>
2023-07-21 20:39:30 -0400

    Hola Mundo

---
+++ src/main/java/proyectofinalbdd1/ProyectoFinalBDD1.java
@@ -0,0 +1,17 @@
+package proyectofinalbdd1;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Juan Urdaneta
+ */
+public class ProyectoFinalBDD1 {
+
+  public static void main(String[] args) {
+    // Crear frame principal
+    JFrame ventanaPrincipal = new ventanas.principal();
+    // Mostrar frame principal
+    ventanaPrincipal.setVisible(true);
+  }
+}
